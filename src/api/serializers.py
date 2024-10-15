3dd3dfc7e185064068ea9ac9fdf2938fc597e998
Danil Galeev <galoffdan@gmail.com>
2024-10-15 21:10:43 +0300

    Add registration with referral code

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -14,6 +14,7 @@ class UserModelSerializer(serializers.ModelSerializer):
 
 class UserRegisterModelSerializer(serializers.ModelSerializer):
     confirm_password = serializers.CharField(required=True, label="Подтверждение пароля", write_only=True)
+    referral_code = serializers.CharField(required=False, write_only=True, label="реферальный код")
 
     def validate(self, attrs):
         attrs = super().validate(attrs)
@@ -22,17 +23,30 @@ class UserRegisterModelSerializer(serializers.ModelSerializer):
         validate_password(attrs["password"])
         return attrs
 
+    def validate_referral_code(self, referral_code):
+        code_qs = ReferralCode.objects.filter(value=referral_code, is_active=True)
+        if not code_qs.exists():
+            raise ValidationError("Реферальный код неверен или неактивен")
+
+        if code_qs.first().is_expired:
+            raise ValidationError("Реферальный код неверен или неактивен")
+
+        return referral_code
+
     def create(self, validated_data):
         validated_data.pop("confirm_password")
+        referral_code = validated_data.pop("referral_code", None)
         user = User(**validated_data)
         user.set_password(validated_data["password"])
+        if referral_code:
+            user.referrer = ReferralCode.objects.get(value=referral_code).user
         user.save()
         return user
 
     class Meta:
         model = User
-        fields = ("email", "password", "confirm_password")
-        extra_kwargs = {"password": {"write_only": True}}
+        fields = ("email", "password", "confirm_password", "referral_code", "referrer")
+        extra_kwargs = {"password": {"write_only": True}, "referrer": {"read_only": True}}
 
 
 class ReferralCodeCreateModelSerializer(serializers.ModelSerializer):
